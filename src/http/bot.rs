ca5be2a49c2599c5cdb7aa42261dcc18b0a1d1f2
DistortedPumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2023-01-27 17:01:49 -0500

    Add http payload/data for creating bot

---
+++ src/http/bot.rs
@@ -0,0 +1,24 @@
+use crate::Maybe;
+use serde::{Deserialize, Serialize};
+#[cfg(feature = "openapi")]
+use utoipa::ToSchema;
+
+/// Payload sent to create a new bot.
+#[derive(Clone, Debug, Deserialize)]
+#[cfg_attr(feature = "client", derive(Serialize))]
+#[cfg_attr(feature = "openapi", derive(ToSchema))]
+pub struct CreateBotPayload {
+    /// The username of the bot. Must be between 2 and 32 characters.
+    pub username: String,
+}
+
+/// Data returned when creating a new bot.
+#[derive(Clone, Debug, Serialize)]
+#[cfg_attr(feature = "client", derive(Deserialize))]
+#[cfg_attr(feature = "openapi", derive(ToSchema))]
+pub struct CreateUserResponse {
+    /// The ID of the bot.
+    pub id: u64,
+    /// The token to use for authentication.
+    pub token: String,
+}
