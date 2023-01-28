a857083a3e4e46c49c0a737ab220348b53bd6a20
DistortedPumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2023-01-28 11:47:07 -0500

    Add delete_bot and make egister_bot return the bot created

--- src/db/bot.rs
+++ src/db/bot.rs
@@ -1,6 +1,6 @@
 use crate::{
     db::{get_pool, user::construct_user},
-    models::user::{User, UserFlags},
+    models::{user::{User, UserFlags}, bot::Bot, MaybePartialUser},
     Error
 };
 
@@ -23,26 +23,32 @@ pub trait BotDbExt<'t>: DbExt<'t> {
         username: impl AsRef<str> + Send,
         
     ) -> crate::Result<()> {
-        let discriminator = sqlx::query!(
+        let user = sqlx::query!(
             "INSERT INTO
                 users (id, username, flags)
             VALUES
                 ($1, $2, $3)
             RETURNING
-                discriminator",
+                *",
             id as i64,
             username.as_ref().trim(),
             UserFlags::BOT,
         )
         .fetch_optional(self.transaction())
-        .await?;
-
-        if discriminator.is_none() {
-            return Err(Error::AlreadyTaken {
-                what: "username".to_string(),
-                message: "Username is already taken".to_string(),
-            });
-        }
+        .await?
+        .map(|u| User {
+            id: u.id,
+            username: u.username,
+            discriminator: u.discriminator,
+            avatar: u.avatar,
+            banner: u.banner,
+            bio: u.bio,
+            flags: UserFlags::from_bits_truncate(u.flags),
+        })
+        .ok_or_else(|| Error::AlreadyTaken {
+            what: "username".to_string(),
+            message: "Username is already taken".to_string(),
+        })?;
 
         sqlx::query!(
             "INSERT INTO bots VALUES ($1, $2)",
@@ -52,6 +58,24 @@ pub trait BotDbExt<'t>: DbExt<'t> {
         .execute(self.transaction())
         .await?;
 
+        Ok((Bot {
+            user: MaybePartialUser::Full(user),
+            owner_id,
+        }))
+    }
+
+    /// Deletes a bot from the database.
+    ///
+    /// This method uses transactions, on the event of an ``Err`` the transaction must be properly
+    /// rolled back, and the transaction must be committed to save the changes.
+    ///
+    /// # Errors
+    /// * If an error occurs with deleting the bot.
+    async fn delete_bot(&mut self, id: u64) -> sqlx::Result<()> {
+        // deleting from the `bots` table is done implicitly due to CASCADE
+        sqlx::query!("DELETE FROM users WHERE id = $1", id as i64)
+            .execute(self.transaction())
+            .await?;
         Ok(())
     }
 
